69e579e0e522093b4abbce1ed325858bbb6e556f
Eir <everythinginreturninfo.com>
2026-05-24 05:01:30 -0400

    completed lesson 2 projects and challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCooldown = 1f;
+    private float nextSpawnTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            Time.time >= nextSpawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            nextSpawnTime = Time.time + spawnCooldown;
+        }
+    }
+}
